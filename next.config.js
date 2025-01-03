d7d7df79c01e2b67e4547dc8ff9c55b2fb640020
Alexandros Mathopoulos <alexandros@mathopoulos.com>
2025-01-03 09:01:18 -0600

    Simplify Next.js and PostCSS configuration

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  experimental: {
-    optimizeCss: true,
-  },
-  webpack: (config) => {
-    config.resolve.fallback = { fs: false };
-    return config;
-  },
 }
 
-module.exports = nextConfig;
+module.exports = nextConfig
--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,6 @@
 module.exports = {
   plugins: {
-    'postcss-import': {},
-    'tailwindcss/nesting': {},
     tailwindcss: {},
     autoprefixer: {},
-    'postcss-preset-env': {
-      features: { 'nesting-rules': false },
-    },
   },
 } 
