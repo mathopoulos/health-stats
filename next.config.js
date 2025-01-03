03ffe8795945e927a0a663343f4472195aa49d6f
Alexandros Mathopoulos <alexandros@mathopoulos.com>
2025-01-02 23:27:07 -0600

    Update Next.js config and add CSS loaders

--- next.config.js
+++ next.config.js
@@ -2,4 +2,14 @@ module.exports = {
   // Add your Next.js configuration options here
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    unoptimized: true,
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader', 'postcss-loader'],
+    });
+    return config;
+  },
 };
