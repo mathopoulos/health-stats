78b452a10995b652d8712786bb62840567d63106
Alexandros Mathopoulos <alexandros@mathopoulos.com>
2025-01-21 19:51:14 -0600

    Fix utils.ts file formatting

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,1 +1,4 @@
-import { exec } from "child_process";\nimport { promisify } from "util";\n\nexport const execAsync = promisify(exec);
+import { exec } from 'child_process';
+import { promisify } from 'util';
+
+export const execAsync = promisify(exec);
